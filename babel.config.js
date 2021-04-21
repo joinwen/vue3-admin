d832f4c1fbdf34038abcf2b781d89e7eab6cf4e6
cywen <18205055638@163.com>
2021-04-21 18:41:17 +0800

    conf babel-import-plugin

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
+const plugins = [
+  [
+    "import",
+    {
+      libraryName: "element-plus",
+      customStyleName: (name) => {
+        return `element-plus/lib/theme-chalk/${name}.css`;
+      },
+    },
+  ],
+];
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins,
 };
