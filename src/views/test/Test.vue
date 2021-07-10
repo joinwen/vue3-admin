4c1debcffe9120a879addbc263ab16178b78b7a7
joinwen <greatawen@smail.hyit.edu.cn>
2021-07-10 12:14:34 +0800

    add: test init

---
+++ src/views/test/Test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "Test",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style lang="less" scoped></style>
